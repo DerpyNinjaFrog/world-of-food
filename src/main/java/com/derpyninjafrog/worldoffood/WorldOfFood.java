de0cc9317dc3b7bb5b1b202f6eb0d0ba09acd2d1
DerpyNinjaFrog <derpyninjafrog@gmail.com>
2022-03-29 15:32:10 -0500

    small edits

--- src/main/java/com/derpyninjafrog/worldoffood/WorldOfFood.java
+++ src/main/java/com/derpyninjafrog/worldoffood/WorldOfFood.java
@@ -2,8 +2,12 @@ package com.derpyninjafrog.worldoffood;
 
 import com.derpyninjafrog.worldoffood.init.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WorldOfFood implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("world_of_food");
 
     //Mod ID
     public static final String MOD_ID = "world_of_food";
