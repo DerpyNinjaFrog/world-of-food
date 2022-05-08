027c9e2b36f5ccfe016d059db5bb90fb0e89c7d0
DerpyNinjaFrog <derpyninjafrog@gmail.com>
2022-05-08 16:01:16 -0500

    Caramel Bucket no longer drinkable

--- src/main/java/com/derpyninjafrog/worldoffood/items/CaramelBucket.java
+++ src/main/java/com/derpyninjafrog/worldoffood/items/CaramelBucket.java
@@ -3,7 +3,7 @@ package com.derpyninjafrog.worldoffood.items;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.*;
 
-public class CaramelBucket extends MilkBucketItem {
+public class CaramelBucket extends Item {
     public CaramelBucket() {
         super(new FabricItemSettings()
                 .group(ItemGroup.FOOD)
